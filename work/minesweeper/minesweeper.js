70d9d7d3cd8d135d1ad4e5dfa456e1974d6a01b2
Anna P <podhajskaa@gmail.com>
2017-06-07 21:34:49 +1200

    added minesweeper o blog work

--- work/minesweeper/minesweeper.js
+++ work/minesweeper/minesweeper.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// var board defined:
+var userWon = false;  //var for playing win sound once
+
 var board = {
   cells: [
     // {row:0,col:0,isMine:true, hidden:true},
@@ -15,9 +16,7 @@ var board = {
   ]
 }
 
-//var board;
-
-function generateBoard(len) {
+function generateBoard(len) {   //ania: generating board above automatically
   board.cells = [];
 
   for (var i = 0; i < len; i++) {
@@ -28,36 +27,14 @@ function generateBoard(len) {
 
   var mineIndex = Math.floor(Math.random()*(len*len));
   board.cells[mineIndex].isMine = true;
-
-
 }
 generateBoard(3);
 
-
-//
-// function BombSet() = {
-//
-//   function randomBoolean() {
-//   var randomNumber = Math.random() >= 0.5;
-//   console.log(randomNumber);
-//   }
-//   // randomBoolean()
-//
-//   for (var i = 0; i<= board.cells[i]; i++) {
-//     board.cells[i].isMine["false"] = randomBoolean()
-//   }
-// }
-// BombSet();
-
-
-
 function startGame () {
-//_________ANIA #6.3:
-
+//_________ania:  waffle #6.3:
   for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
-
   document.getElementsByClassName('board')[0].addEventListener('click', checkForWin);
   document.getElementsByClassName('board')[0].addEventListener('contextmenu', checkForWin);
 
@@ -73,14 +50,19 @@ function startGame () {
 
 
 function checkForWin (evt) {
-  if (evt.target.classList.contains('mine')) { //inspired by EDA's lib
+
+  if (userWon === true) {  //ania: if user already won and the sound played, do nothing
+    return;
+  }
+
+  if (evt.target.classList.contains('mine')) { //ania: inspired by EDA's lib
     document.getElementById("sound-loose").play();
   }
-  else if (evt.currentTarget.className==="board") { //evt.currentTarget.className is "board" taken from debugger
+  else if (evt.currentTarget.className==="board") { //ania: evt.currentTarget.className is "board" taken from debugger
     document.getElementById("sound-click").play();
   }
 
-  //_______ANIA #6.3:
+  //__ania #6.3:
  //assume that total mines and total correctly marked must be equal:
 
   var totalMines = 0;
@@ -89,10 +71,10 @@ function checkForWin (evt) {
   for (var i = 0; i < board.cells.length; i++) {
 
     if (board.cells[i].isMine && board.cells[i].isMarked) {
-      totalCorrectlyMarked +=1; //if a mine exists and is marked blue correctly. .Marked prop. is added by another function done by eda
+      totalCorrectlyMarked +=1; //ania: if a mine exists and is marked blue correctly. .Marked prop. is added by another function done by eda
     }
     else if (board.cells[i].hidden === true) {
-      return;  //note: a square can be both .isMarked set to true and .hidden set to true at the same time.
+      return;  //ania: bug in waffle task a square can be both .isMarked set to true and .hidden set to true at the same time.
     }
 
     if (board.cells[i].isMine === true) {
@@ -103,22 +85,10 @@ function checkForWin (evt) {
   if (totalMines === totalCorrectlyMarked) {
     lib.displayMessage('You won!');
     document.getElementById("sound-win").play();
+    userWon = true;
   }
-
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`:
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through
-// them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
   //_______ANIA:
   var surrounding = lib.getSurroundingCells(cell.row, cell.col);
